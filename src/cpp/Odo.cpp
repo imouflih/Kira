94bef79c8f6472c2c4d374de15d8cc4d0b8a23ae
ILIASSE MOUFLIH <iliasse.mofloh@gmail.com>
2023-03-25 15:29:54 +0100

    add a trajectory correction function

--- src/cpp/Odo.cpp
+++ src/cpp/Odo.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <termios.h>
+#include <chrono>
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/i2c-dev.h>
@@ -20,7 +21,11 @@
 // Control config
 #define THETA_TOLERANCE 0.03 // rad
 
-const char* I2C_BUS = "/dev/i2c-1";
+#define MAX_SPEED 50
+#define MIN_SPEED 7
+
+const char* I2C_BUS = "/dev/i2c-1"; // I2C bus
+const float Kp = 0.1; // Proportional gain
 
 int file = open(I2C_BUS, O_RDWR);
 
@@ -28,45 +33,54 @@ int countLeft;
 int countRight;
 
 // Global variables
-float x = 0;                   // x position of the robot
-float y = 0;                   // y position of the robot
-float theta = 0;             // orientation of the robot
-int countLeft_previous = 0;  // previous count of left encoder
-int countRight_previous = 0; // previous count of right encoder
+float x;                   // x position of the robot
+float y;                   // y position of the robot
+float theta;             // orientation of the robot
+int countLeft_previous;  // previous count of left encoder
+int countRight_previous; // previous count of right encoder
+
+int speed;
 
 uint8_t data[10];
 uint8_t data_to_send[4];
 
 void init() {
-    countLeft_previous = 0;
-    countRight_previous = 0;
+    unsigned char b = INIT_COUNTERS;
+    if (write(file, &b, 1) == -1) {
+        printf("Failed to write to I2C bus. -init\n");
+    }
+    x = y = theta = 0;
+    countLeft_previous = countRight_previous = 0;
 }
 
 void orderMove(int speedRight, int speedLeft) {
+    // make sure speed doesn't exceed the max and min speed
+    speedRight = (speedRight != 0) ? ((speedRight > 0) ? 1 : -1) * std::max(MIN_SPEED, std::min(abs(speedRight), MAX_SPEED)) : 0;
+    speedLeft = (speedLeft != 0) ? ((speedLeft > 0) ? 1 : -1) * std::max(MIN_SPEED, std::min(abs(speedLeft), MAX_SPEED)) : 0;
+
+    // send data to arduino
     data_to_send[0] = speedLeft & 0xFF;
     data_to_send[1] = (speedLeft >> 8) & 0xFF;
     data_to_send[2] = speedRight & 0xFF;
     data_to_send[3] = (speedRight >> 8) & 0xFF;
+
     if (file < 0) {
         printf("Failed to open I2C device.\n");
         return;
     }
-
     if (ioctl(file, I2C_SLAVE, ADDRESS_I2C) < 0) {
         printf("Failed to set I2C address.\n");
         return;
     }
     if (write(file, data_to_send, 4) == -1) {
-        printf("Failed to write to I2C bus.\n");
+        printf("Failed to write to I2C bus. -orderMove\n");
         return;
     }
 }
 
-// Function that update (x,y,theta) of the robot
-void update_position() {
-
+void getCounts() {
     if (read(file, data, 10) != 10) {
-        perror("Failed to read data from i2c bus");
+        perror("Failed to read data from i2c bus. -getCounts");
         exit(1);
     }
 
@@ -76,6 +90,27 @@ void update_position() {
 
     countLeft = data[1] | (data[2] << 8) | (data[3] << 16) | (data[4] << 24);
     countRight = data[5] | (data[6] << 8) | (data[7] << 16) | (data[8] << 24);
+}
+
+void correctorSpeed(int leftSpeed, int rightSpeed, long differenceCounts) {
+    // Calculate the error between left and right encoder counts
+    long error = countRight - countLeft - differenceCounts;
+
+    // Calculate the speed correction using proportional gain
+    float correction = Kp * error;
+
+    // Apply the correction to the motor speeds
+    float correctedLeftSpeed = static_cast<int>(round(leftSpeed + correction));
+    float correctedRightSpeed = static_cast<int>(round(rightSpeed - correction));
+
+    // Set the corrected motor speeds
+    orderMove(correctedLeftSpeed, correctedRightSpeed);
+}
+
+// Function that update (x,y,theta) of the robot
+void update_position() {
+
+    getCounts();
 
     // Calculate the distance traveled by each wheel
     float distanceLeft = (countLeft - countLeft_previous) * 2 * M_PI * DIAMETER / TICK_PER_TURN;
@@ -85,7 +120,7 @@ void update_position() {
     float distanceAverage = (distanceLeft + distanceRight) / 2;
 
     // Calculate the angle turned by the robot
-    float angle = atan2((distanceRight - distanceLeft), ENTRAXE);
+    float angle = (distanceRight - distanceLeft) / ENTRAXE;
 
     // Calculate the change in position of the robot
     float delta_x = distanceAverage * cos(theta + angle / 2);
@@ -98,7 +133,7 @@ void update_position() {
     theta += delta_theta;
 
     // Make sure that theta is between 0 and 4*PI
-    theta = (fmod(theta, 2 * M_PI) < 0 ? fmod(theta, 2 * M_PI) + 2 * M_PI : fmod(theta, 2 * M_PI));
+    theta = (fmod(theta, 4 * M_PI) < 0 ? fmod(theta, 4 * M_PI) + 4 * M_PI : fmod(theta, 4 * M_PI));
 
     // Update the previous count values
     countLeft_previous = countLeft;
@@ -107,25 +142,26 @@ void update_position() {
     std::printf("\033[2J"); // Clear the screen
     std::printf("\033[0;0H"); // Move the cursor to the top-left corner
     std::printf("countLeft = %d, countRight = %d", countLeft, countRight);
-    std::printf("\nx = %.2f, y = %.2f, theta = %.2f\n", (int)x, (int)y, theta);
+    std::printf("\nx = %.2f, y = %.2f, theta = %.2f\n", x, y, theta);
     std::fflush(stdout); // Flush stdout to ensure the output is printed immediately
+
+    // sleep(0.1);
 
 }
 
 // Function to rotate the robot
 void setTheta(float theta_target) {
 
-    int speed = 25;
+    int speed = 15;
 
     // Get the current angle of the robot
     update_position();
 
-    // Continue adjusting the angle until it is within the specified tolerance
-    while (fmod(fabs(theta_target - theta), 2 * M_PI) > THETA_TOLERANCE) {
+    // Rotate the robot, make sure that he always takes the shortest path
+    fabs(theta_target - theta) < 2 * M_PI ? theta_target > theta ? orderMove(-speed, speed) : orderMove(speed, -speed) : theta_target > theta ? orderMove(speed, -speed) : orderMove(-speed, speed);
 
-        // Rotate the robot, make sure that he always takes the shortest path
-        fabs(theta_target - theta) < M_PI ? theta_target > theta ? orderMove(-speed, speed) : orderMove(speed, -speed) : theta_target > theta ? orderMove(speed, -speed) : orderMove(-speed, speed);
-        // orderMove(speed, -speed);
+    // Continue adjusting the angle until it is within the specified tolerance
+    while (fmod(fabs(theta_target - theta), 4 * M_PI) > THETA_TOLERANCE) {
 
         // Update the current angle of the robot
         update_position();
@@ -146,12 +182,14 @@ void go_to(int x_target, int y_target) {
     int dy = y_target - y_current;
     float distance = sqrt(dx * dx + dy * dy);
     float angle = atan2(dy, dx);
-    angle = fmod(angle, 2 * M_PI) < 0 ? fmod(angle, 2 * M_PI) + 4 * M_PI : fmod(angle, 2 * M_PI);
-
-    int speed = 15;
+    angle = fmod(angle, 4 * M_PI) < 0 ? fmod(angle, 4 * M_PI) + 4 * M_PI : fmod(angle, 4 * M_PI);
 
     // Turn the robot towards the target position
     setTheta(angle);
+
+    update_position();
+
+    long diff = countRight - countLeft;
 
     orderMove(speed, speed);
 
@@ -166,6 +204,7 @@ void go_to(int x_target, int y_target) {
         dx = x_target - x_current;
         dy = y_target - y_current;
         distance = sqrt(dx * dx + dy * dy);
+        correctorSpeed(speed, speed, diff);
         // angle = atan2(dy, dx) * 180.0 / M_PI;
         // angle = fmod(angle, 2 * M_PI) < 0 ? fmod(angle, 2 * M_PI) + 2 * M_PI : fmod(angle, 2 * M_PI);
 
@@ -181,29 +220,7 @@ void go_to(int x_target, int y_target) {
     setTheta(angle_init);
 }
 
-void enableRawMode() {
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO | ICANON);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-}
-
-void disableRawMode() {
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag |= (ECHO | ICANON);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-}
-
 int main(int argc, char** argv) {
-    if (argc == 3) {
-        int speedLeft, speedRight;
-        speedLeft = atoi(argv[1]);
-        speedRight = atoi(argv[2]);
-        printf("speed left : %d\nspeed right : %d\n", speedLeft, speedRight);
-        orderMove(speedLeft, speedRight);
-        return 0;
-    }
     if (file < 0) {
         perror("Failed to open i2c bus");
         exit(1);
@@ -213,28 +230,36 @@ int main(int argc, char** argv) {
         perror("Failed to acquire bus access or talk to slave");
         exit(1);
     }
-    unsigned char b = INIT_COUNTERS;
-    if (write(file, &b, 1) == -1) {
-        printf("Failed to write to I2C bus.\n");
-        exit(1);
+    speed = 50;
+    if (argc == 3) {
+        // init();
+        sleep(1);
+        int speedLeft, speedRight;
+        speedLeft = atoi(argv[1]);
+        speedRight = atoi(argv[2]);
+        orderMove(speedLeft, speedRight);
+        update_position();
+        long diff = countRight - countLeft;
+        sleep(3);
+        while (true) {
+            correctorSpeed(speedLeft, speedRight, diff);
+            update_position();
+        }
+        return 0;
+    }
+    if (argc == 4) {
+        init();
+        sleep(1);
+        int speedLeft, speedRight;
+        speedLeft = atoi(argv[1]);
+        speedRight = atoi(argv[2]);
+        orderMove(speedLeft, speedRight);
+        printf("speed left : %d\nspeed right : %d\n", speedLeft, speedRight);
+        return 0;
     }
     init();
     sleep(1);
 
-    enableRawMode();
-    // je dois faire une initialisation des counts
-
-    setTheta(M_PI);
-    setTheta(0);
-    // go_to(800, 800);
-
-    // while (true) {
-    //     // std::cout << "Left ticks: " << countLeft << ", Right ticks: " << countRight << std::endl;
-    //     update_position();
-    //     // std::cout << "x: " << position.x << ", y: " << position.y << ", theta: " << position.theta << std::endl;
-    //     sleep(0.1);
-    // }
-
-    disableRawMode();
+    go_to(5000, 0);
     return 0;
 }
